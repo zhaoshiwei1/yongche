74eeef977a0001539d3b60f135a0aa001d59df4f
zhaoshiwei1 <447518646@qq.com>
2017-02-22 11:18:59 +0800

    recovery

--- com.yongche.api.easysettlement/src/global/GlobalDefine.java
+++ com.yongche.api.easysettlement/src/global/GlobalDefine.java
@@ -7,13 +7,13 @@ public class GlobalDefine
 	public static final boolean out_put = false;
 	public static final boolean out_to_file = true;
 	//Ω·À„ ˝æ›ø‚≈‰÷√
-	public static final String db_url = "jdbc:mysql://10.0.11.141/"; 
     public static final String name = "com.mysql.jdbc.Driver";  
+	
+	public static final String db_url = "jdbc:mysql://10.0.11.141/"; 
     public static final String user = "yongche";  
     public static final String password = ""; 
     
 	public static final String db_order = "jdbc:mysql://10.0.11.159/"; 
-    public static final String db_name = "com.mysql.jdbc.Driver";  
     public static final String db_user = "order";  
     public static final String db_password = "order"; 
     
